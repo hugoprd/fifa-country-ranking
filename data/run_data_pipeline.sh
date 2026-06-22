68820c73437de262c83beb948e94c2af9bfe9de1
hugoprd <hugo.prd.br@gmail.com>
2026-06-22 17:25:49 -0300

    feat: Implementa escolha para o usuário de fazer cada processo ou não

--- data/run_data_pipeline.sh
+++ data/run_data_pipeline.sh
@@ -1,37 +1,72 @@
 #!/usr/bin/env bash
 
-#########################
-### RUN DATA PIPELINE ###
-#########################
-
-# ==============================================================================
-# Critical Safety Barrier (Fail-Fast):
-# the 'set -e' makes the bash script stop immediately if any 
-# python script returns an error. this prevents the domino effect of failures.
-# ==============================================================================
+# 'set-e' makes the script stop if any command fails
 set -e
 
-# discover the absolute directory where this bash script is saved.
-# this ensures that you can run the command from any location in the terminal
-# and it will always find the correct Python files.
+# absolute dir of 'data' folder
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "==============================================================="
-echo "        STARTING DATA PIPELINE FOR FIFA COUNTRY RANKING        "
-echo "==============================================================="
+echo "======================================================"
+echo "           STARTING FIFA DATA PIPELINE                "
+echo "======================================================"
 
-echo -e "\n---> [1/4] Extracting External Metadata (Wikipedia)..."
-python "$SCRIPT_DIR/extract_external_metadata.py"
+### =====================================================================================================================
+# verifies if the validation file exists. if yes, ask before running the script again. if not, run the script directly
+### =====================================================================================================================
+run_step() {
+    local step_title="$1"
+    local python_script="$2"
+    local validation_file="$3"
 
-echo -e "\n---> [2/4] Extracting Raw Data (Club World Cup)..."
-python "$SCRIPT_DIR/extract_data.py"
+    echo -e "\n---> $step_title"
 
-echo -e "\n---> [3/4] Processing Data and Calculating Weights..."
-python "$SCRIPT_DIR/process_data.py"
+    if [ -f "$validation_file" ]; then
+        echo "   [!] The data for this step already exists in the directory."
+        # read -p ask a quesstion on the terminal. the common is N (no)
+        read -p "   Would you like to run this script again? (y/N): " answer
+        
+        # if the user writes 'y' or 'Y', run the script
+        if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+            echo "   Executando $python_script..."
+            python "$python_script"
+        else
+            echo "   >> Pulando etapa (Cache mantido)."
+        fi
+    else
+        # if the file doesn't exist, run the script directly without asking
+        echo "   [!] Data not found. Executing script..."
+        python "$python_script"
+    fi
+}
 
-echo -e "\n---> [4/4] Refining Final Data..."
-python "$SCRIPT_DIR/refine_data.py"
+### ==============================================================================
+# RUNNING THE STEPS
+### ==============================================================================
 
-echo -e "\n===================================================================="
-echo "                 PIPELINE CONCLUDED SUCCESSFULLY.                  "
-echo "===================================================================="
+# 1: EXTRACTION Wikipedia
+run_step \
+    "[1/4] Extracting External Metadata (Wikipedia)..." \
+    "$SCRIPT_DIR/extract_external_metadata.py" \
+    "$SCRIPT_DIR/external_metadata/world_teams_metadata.csv"
+
+# 2: EXTRACTION FBref e FootyStats
+run_step \
+    "[2/4] Extracting Raw Data (World Cup/FBref)..." \
+    "$SCRIPT_DIR/extract_data.py" \
+    "$SCRIPT_DIR/raw/fbref_world_cup_games.csv"
+
+# 3: PROCESSING AND ENRICHMENT
+run_step \
+    "[3/4] Processing Data, Clubs and Weights..." \
+    "$SCRIPT_DIR/process_data.py" \
+    "$SCRIPT_DIR/processed/processed_players.csv"
+
+# 4: REFINEMENT AND FEATURE ENGINEERING
+run_step \
+    "[4/4] Calculating Efficiency and Synergy (Machine Learning)..." \
+    "$SCRIPT_DIR/refine_data.py" \
+    "$SCRIPT_DIR/refined/ml_national_synergy_features.csv"
+
+echo -e "\n=========================================================="
+echo "         DATA PIPENLINE CONCLUDED SUCCESSFULLY           "
+echo "=========================================================="
