cb2218cf02c5c7285c6ff63784f5ac8752338c54
hugoprd <hugo.prd.br@gmail.com>
2026-06-25 19:13:33 -0300

    comentando alterações apenas para baixar projeto

--- ml_model_scripts/utils/model_extensions.py
+++ ml_model_scripts/utils/model_extensions.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import pandas as pd
 from torch.utils.data import Dataset
+import itertools
 
 """
 File that it's going to be used to create custom layers or attention mechanisms that can be used in the model.
@@ -163,3 +164,74 @@ class FIFANationalTeamDataset(Dataset):
         target = torch.tensor([target_row["avg_synergy_score"]], dtype=torch.float32)
 
         return player_tensor, synergy_tensor, padding_mask, target
+
+
+class TacticalOptimizer:
+    """
+    Simulates the tatical search and perfect escalation of a national team,
+    respecting the football position rules
+    """
+
+    def __init__(self, trained_model):
+        self.model = trained_model
+
+        # Dicionário definindo a quantidade exata de posições por tática
+        self.tactics_map = {
+            "4-3-3": {"GK": 1, "DEF": 4, "MID": 3, "ATT": 3},
+            "4-4-2": {"GK": 1, "DEF": 4, "MID": 4, "ATT": 2},
+            "3-5-2": {"GK": 1, "DEF": 3, "MID": 5, "ATT": 2},
+            "5-4-1": {"GK": 1, "DEF": 5, "MID": 4, "ATT": 1},
+        }
+
+    def find_best_lineup(self, df_country_squad: pd.DataFrame):
+        """
+        Receives a DataFrame withh all players of a unique national team (e.g.: 23 players).
+        The table have to have the column 'position' (GK, DEF, MID, ATT) and the features/ID column
+        """
+        best_score = -float("inf")
+        best_tactic = None
+        best_11 = None
+
+        # groups the available players by position
+        gk_pool = df_country_squad[df_country_squad["position"] == "GK"]
+        def_pool = df_country_squad[df_country_squad["position"] == "DEF"]
+        mid_pool = df_country_squad[df_country_squad["position"] == "MID"]
+        att_pool = df_country_squad[df_country_squad["position"] == "ATT"]
+
+        # tests each possible tatic
+        for tactic_name, req in self.tactics_map.items():
+            # generates all possible combinations for this tatic
+            gk_combs = list(itertools.combinations(gk_pool.index, req["GK"]))
+            def_combs = list(itertools.combinations(def_pool.index, req["DEF"]))
+            mid_combs = list(itertools.combinations(mid_pool.index, req["MID"]))
+            att_combs = list(itertools.combinations(att_pool.index, req["ATT"]))
+
+            # cartesian product (tests all the possible defenses with all possible means
+            for g, d, m, a in itertools.product(gk_combs, def_combs, mid_combs, att_combs):
+                # 11 players of this especific combination
+                lineup_indices = list(g + d + m + a)
+                df_lineup = df_country_squad.loc[lineup_indices]
+
+                # 1. converts the features of the 11 players in Tensor
+                # 2. rebuild the synergy matrix to only these 11
+                players_tensor, synergy_matrix = self._prepare_tensors(df_lineup)
+
+                with torch.no_grad():
+                    # Batch=1, no padding
+                    score = self.model(players_tensor.unsqueeze(0), synergy_matrix.unsqueeze(0))
+                    current_score = score.item()
+
+                # if the model point for this team be bigger than the best point saved until now
+                if current_score > best_score:
+                    best_score = current_score
+                    best_tactic = tactic_name
+                    best_11 = df_lineup["player_name"].tolist()
+
+        return best_tactic, best_score, best_11
+
+    def _prepare_tensors(self, df_lineup):
+        """Função fictícia: Você precisa transformar o DataFrame filtrado de volta em Tensor"""
+        # players_tensor = torch.tensor(df_lineup[['feat1', 'feat2'...]].values, dtype=torch.float32)
+        # synergy_matrix = create_synergy_graph(df_lineup)
+        # return players_tensor, synergy_matrix
+        pass
